2e61f0b394b315f073fac9f2f2a1c79a57c1a940
Robin <royrobin202@gmail.com>
2024-11-06 20:37:40 +0530

    CSRF Token

---
+++ Spring_Boot_Security/SpringSecEx/src/main/java/com/robin/SpringSecEx/StudentController.java
@@ -0,0 +1,35 @@
+package com.robin.SpringSecEx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class StudentController {
+	
+	private List<Student> students = new ArrayList<>(List.of(new Student(1,"Robin", 90),new Student(1,"rohan", 80)));
+	
+	@GetMapping("/Students")
+	public List<Student> getStudents(){
+		return students;
+	}
+	
+	@GetMapping("/csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
+	
+	@PostMapping("/Students")
+	public Student addStudent(@RequestBody Student student){
+		students.add(student);
+		return student;
+	}
+	
+}
