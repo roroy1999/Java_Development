d71ae01361b25e882b8d484e8bda878bd3217be2
Robin <royrobin202@gmail.com>
2026-02-16 23:32:32 +0530

    Mono Implementation

--- Spring_Reactive_programing_learning/ReactiveProject/src/test/java/com/reactive/app/ReactiveProjectApplicationTests.java
+++ Spring_Reactive_programing_learning/ReactiveProject/src/test/java/com/reactive/app/ReactiveProjectApplicationTests.java
@@ -1,13 +1,55 @@
 package com.reactive.app;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class ReactiveProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void workingWithMonoUsingSubscriber(){
+//		Mono ----> Publisher that have 0...1 items
+		Mono<String> learn = Mono.just("Learn Code");//creates mono of String type since we passed String
+		learn.subscribe(new Subscriber<String>() {
+			@Override
+			public void onSubscribe(Subscription s) {
+				s.request(1);
+			}
+
+			@Override
+			public void onNext(String s) {
+				System.out.println("Hello");
+			}
+
+			@Override
+			public void onError(Throwable t) {
+
+			}
+
+			@Override
+			public void onComplete() {
+
+			}
+		});
+		System.out.println("Testing");
+	}
+
+	@Test
+	void workingWithMonoUsingConsumer(){
+//		Mono ----> Publisher that have 0...1 items
+		Mono<String> learn = Mono.just("Learn Code")
+				.log();//creates mono of String type since we passed String
+		learn.subscribe(data->{
+			System.out.println("data is "+ data);
+		});
+		System.out.println("Testing");
 	}
 
 }
